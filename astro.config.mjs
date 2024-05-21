77331e3d9c253346e694dd6213e97e114944a600
root <root@log.cash>
2024-05-21 19:27:55 +0000

    your message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), auth()]
+});
