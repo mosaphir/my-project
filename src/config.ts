84ab5dbbbca98acbe8e9563853ea4ee03325b51e
Saikot Hasan <163502629+mosaphir@users.noreply.github.com>
2024-05-22 01:40:06 +0600

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+// file: src/config.ts
+export const SITE = {
+  website: "https://astro-paper.pages.dev/",
+  author: "Sat Naing",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  title: "AstroPaper",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true, // true by default
+  postPerPage: 3,
+};
